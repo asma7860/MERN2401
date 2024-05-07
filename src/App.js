0d417ddb543ed4dee67efb406d73884fa17eb78c
madhuri9703 <burakamadhuri@gmail.com>
2024-05-07 12:30:20 +0530

    added few images

--- src/App.js
+++ src/App.js
@@ -4,20 +4,56 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>Royal Challengers Benguluru</h1>
+    <br></br>
+    <div className='players'>
+      <img  src="https://bcciplayerimages.s3.ap-south-1.amazonaws.com/ipl/IPLHeadshot2024/94.png"></img>
+      <hr/>
+      <h2>Faf du Plessis</h2>
+      <h2>Batter 🏏</h2>
+    </div>
+    <div className='players'>
+      <img  src="https://bcciplayerimages.s3.ap-south-1.amazonaws.com/ipl/IPLHeadshot2024/597.png"></img>
+      <hr/>
+      <h2>Rajat Patidar</h2>
+      <h2>Batter 🏏</h2>
+    </div>
+    <div className='players'>
+      <img  src="https://bcciplayerimages.s3.ap-south-1.amazonaws.com/ipl/IPLHeadshot2024/2.png"></img>
+      <hr/>
+      <h2>Virat Kohli</h2>
+      <h2>Batter 🏏</h2>
+    </div>
+    <div className='players'>
+      <img  src="https://bcciplayerimages.s3.ap-south-1.amazonaws.com/ipl/IPLHeadshot2024/1007.png"></img>
+      <hr/>
+      <h2>Akash Deep</h2>
+      <h2>Bowler 🏏</h2>
+    </div>
+    <div className='players'>
+      <img  src="https://bcciplayerimages.s3.ap-south-1.amazonaws.com/ipl/IPLHeadshot2024/63.png"></img>
+      <hr/>
+      <h2>Mohammed Siraj</h2>
+      <h2>Bowler 🏏</h2>
+    </div>
+    <div className='players'>
+      <img  src="https://bcciplayerimages.s3.ap-south-1.amazonaws.com/ipl/IPLHeadshot2024/978.png"></img>
+      <hr/>
+      <h2>Yash Dayal</h2>
+      <h2>Bowler 🏏</h2>
+    </div>
+    <div className='players'>
+      <img  src="https://bcciplayerimages.s3.ap-south-1.amazonaws.com/ipl/IPLHeadshot2024/28.png"></img>
+      <hr></hr>
+      <h2>Glenn Maxwell</h2>
+      <h2>AllRounder 🏏</h2>
+    </div>
+    <div className='players'>
+      <img  src="https://bcciplayerimages.s3.ap-south-1.amazonaws.com/ipl/IPLHeadshot2024/1485.png"></img>
+      <hr></hr>
+      <h2>Manoj Bhandage</h2>
+      <h2>AllRounder 🏏</h2>
+    </div>
     </div>
   );
 }
