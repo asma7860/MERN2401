ebf06c84137a955b1e46da62b99979286c00649a
madhuri9703 <burakamadhuri@gmail.com>
2024-05-07 15:43:24 +0530

    added few more

--- src/App.js
+++ src/App.js
@@ -4,56 +4,56 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-    <h1>Royal Challengers Benguluru</h1>
+    <h1>Royal Challengers Bengaluru</h1>
     <img style={{width:"100px", height:"100px", position:"relative", top:"10px"}} src="https://i.pinimg.com/originals/7c/d7/eb/7cd7ebcfa9f8d976b74535181907fe79.png"></img>
     <br></br>
     <div className='players'>
       <img  src="https://bcciplayerimages.s3.ap-south-1.amazonaws.com/ipl/IPLHeadshot2024/94.png"></img>
       <hr/>
       <h2>Faf du Plessis</h2>
-      <h2>Batter 🏏</h2>
+      <h2 className='RCB'>Batter 🏏</h2>
     </div>
     <div className='players'>
       <img  src="https://bcciplayerimages.s3.ap-south-1.amazonaws.com/ipl/IPLHeadshot2024/597.png"></img>
       <hr/>
       <h2>Rajat Patidar</h2>
-      <h2>Batter 🏏</h2>
+      <h2 className='RCB'>Batter 🏏</h2>
     </div>
     <div className='players'>
       <img  src="https://bcciplayerimages.s3.ap-south-1.amazonaws.com/ipl/IPLHeadshot2024/2.png"></img>
       <hr/>
       <h2>Virat Kohli</h2>
-      <h2>Batter 🏏</h2>
+      <h2 className='RCB'>Batter 🏏</h2>
     </div>
     <div className='players'>
       <img  src="https://bcciplayerimages.s3.ap-south-1.amazonaws.com/ipl/IPLHeadshot2024/1007.png"></img>
       <hr/>
       <h2>Akash Deep</h2>
-      <h2>Bowler 🏏</h2>
+      <h2 className='RCB'>Bowler 🏏</h2>
     </div>
     <div className='players'>
       <img  src="https://bcciplayerimages.s3.ap-south-1.amazonaws.com/ipl/IPLHeadshot2024/63.png"></img>
       <hr/>
       <h2>Mohammed Siraj</h2>
-      <h2>Bowler 🏏</h2>
+      <h2 className='RCB' >Bowler 🏏</h2>
     </div>
     <div className='players'>
       <img  src="https://bcciplayerimages.s3.ap-south-1.amazonaws.com/ipl/IPLHeadshot2024/978.png"></img>
       <hr/>
       <h2>Yash Dayal</h2>
-      <h2>Bowler 🏏</h2>
+      <h2 className='RCB'>Bowler 🏏</h2>
     </div>
     <div className='players'>
       <img  src="https://bcciplayerimages.s3.ap-south-1.amazonaws.com/ipl/IPLHeadshot2024/28.png"></img>
       <hr></hr>
       <h2>Glenn Maxwell</h2>
-      <h2>AllRounder 🏏</h2>
+      <h2 className='RCB'>AllRounder 🏏</h2>
     </div>
     <div className='players'>
       <img  src="https://bcciplayerimages.s3.ap-south-1.amazonaws.com/ipl/IPLHeadshot2024/1485.png"></img>
       <hr></hr>
       <h2>Manoj Bhandage</h2>
-      <h2>AllRounder 🏏</h2>
+      <h2 className='RCB'>AllRounder 🏏</h2>
     </div>
     <hr/>  
     <h1 className='main'>Sunrises Hyderabad</h1>
